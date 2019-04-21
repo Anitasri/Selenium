8e8c8acd418baeeed652daacaccf272e3c5dc5a9
IP320 red <IP320 red@ANITA-SRI>
2019-04-22 00:45:49 +0530

    Manual webdriver management for 3 drivers.

---
+++ SeleniumFramework/src/test/java/BrowserTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class BrowserTest {
+	
+	public static void main(String args[])
+	{
+		String projectPath=System.getProperty("user.dir");
+		System.out.println("ProjectPath:"+projectPath);
+		
+		//System.setProperty("webdriver.gecko.driver",projectPath +"//drivers/geckodriver/geckodriver.exe");
+		//WebDriver driver= new FirefoxDriver();
+		
+		//System.setProperty("webdriver.chrome.driver",projectPath +"//drivers/chromedriver/chromedriver.exe");
+		//WebDriver driver= new ChromeDriver();
+		
+		System.setProperty("webdriver.ie.driver",projectPath +"//drivers/iedriver/IEDriverServer.exe");
+		WebDriver driver= new InternetExplorerDriver();
+		
+		driver.get("https://mvnrepository.com/artifact/com.h2database/");
+		driver.quit();
+		
+		
+	}
+
+}
