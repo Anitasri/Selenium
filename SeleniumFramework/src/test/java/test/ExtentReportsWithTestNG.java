71ff059f55e8d6c174e24b7fa295b0379432a5e7
Anitasri <43494217+Anitasri@users.noreply.github.com>
2019-04-25 09:21:52 +0530

    TestNG with Extent reports demo.

--- SeleniumFramework/src/test/java/test/ExtentReportsWithTestNG.java
+++ SeleniumFramework/src/test/java/test/ExtentReportsWithTestNG.java
@@ -2,8 +2,12 @@ package test;
 
 import java.io.IOException;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
@@ -18,21 +22,42 @@ public class ExtentReportsWithTestNG {
 	ExtentReports extent=null;
 	ExtentTest test=null;
 
+	WebDriver driver=null;
+
 	@BeforeSuite  //Run only once before all test cases
 	public void setUp() {
 
 		// start reporters
-		htmlReporter = new ExtentHtmlReporter("extent.html");
+		htmlReporter = new ExtentHtmlReporter("extentReport.html");
 
 		// create ExtentReports and attach reporter(s)
 		extent = new ExtentReports();
 		extent.attachReporter(htmlReporter);
+
+
+
 	}
+
+	@BeforeTest
+	public void setUpTest() {
+
+		String projectPath=System.getProperty("user.dir");
+		System.out.println("ProjectPath:"+projectPath);
+		System.setProperty("webdriver.chrome.driver",projectPath +"//drivers/chromedriver/chromedriver.exe");
+		driver= new ChromeDriver();
+	}
+
+
 
 	@Test
 	public void test() {
 
 		test = extent.createTest("Bing Search", "This is a Sample Test");
+
+		//goto google.com
+		driver.get("https://bing.com/");
+		test.pass("Navigated successfully");
+
 
 		// log(Status, details)
 		test.log(Status.INFO, "This step shows usage of log(status, details)");
@@ -56,6 +81,15 @@ public class ExtentReportsWithTestNG {
 			e.printStackTrace();
 		}
 
+	}
+
+	@AfterTest
+	public void tearDownTest( ) {
+
+		//close the browser
+		driver.close();
+
+		System.out.println("Test google search is completed");
 	}
 
 	@AfterSuite
